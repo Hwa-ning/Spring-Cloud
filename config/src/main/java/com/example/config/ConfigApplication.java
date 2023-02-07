1cb5be62745de7269814665db165bc8df1e53b3f
Hwa-ning <hwaning1@douzone.com>
2023-02-07 13:18:40 +0900

    first commit

---
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -0,0 +1,8 @@
+package com.example.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+}
